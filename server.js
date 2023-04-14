19b846bdb7d9bc365043b48f6ed98058c366f56a
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-04-14 09:47:23 -0700

    working it

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express();
 const port = 4040
 
 
+app.get('/everything', function(req, res) {
+    console.log("everything")
+  res.sendFile(__dirname + '/everything.json');
+});
+app.get('/fold2', function(req, res) {
+  console.log("fold2")
+  res.sendFile(path.join(__dirname, '/fold2.html'));
+});
+app.get('/fold', function(req, res) {
+  console.log("fold")
+  res.sendFile(path.join(__dirname, '/fold.html'));
+});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
