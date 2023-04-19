293dc687a24a714bb4488095f507fc6f049fd48a
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-04-19 16:07:50 -0700

    working it

--- x.js
+++ x.js
@@ -1,42 +1,20 @@
 const everything = require("./everything.json")
-
-function flatten(objectToFlatten) {
-    // This will not be used in the logic of the page - but it will be helpful to put this page together
-    function flattenObject(obj) {
-        const accumulator = {};
-        for (let key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                if ((typeof obj[key]) == 'object' && obj[key] != null) {
-                    const flatObject = flattenObject(obj[key]);
-                    for (let x in flatObject) {
-                        if (flatObject.hasOwnProperty(x)) {
-                            accumulator[key + '.' + x] = flatObject[x];
-                        }
-                    }
-                } else {
-                    accumulator[key] = obj[key];
-                }
-            }
-        }
-        return accumulator;
-    }
-    return flattenObject(objectToFlatten)
+const lowerCaseLeadingKey = ((str) => {
+    const components = str.split('.');
+    components[0] = components[0].toLowerCase();
+    return components.join('.');
+})
+const len = (map)=> { 
+    return Object.keys(map).length
 }
-let headwaters = []
-function init() {
-    for (let k in everything) {
-        headwaters.push(k)
-    }
-}
-
-function step1_roll(thing, parent, history, loop, result) {
+function step1_getCategoricalOptionalityObjects(thing, parent, history, loop, result) {
     if (parent.length > 0) {
         history += parent + "."
     }
     if (typeof thing === "object") {
         for (let k in thing) {
             if (thing[k] !== undefined) {
-                step1_roll(thing[k], k, history, ++loop, result)
+                step1_getCategoricalOptionalityObjects(thing[k], k, history, ++loop, result)
             }
         }
     } else {
@@ -45,7 +23,6 @@ function step1_roll(thing, parent, history, loop, result) {
         result[history] = { mandatory: thing, type }
     }
 }
-
 
 function step2_getNeededFirstClassObjects(categoricalOptionalityObject) {
     const paths = Object.keys(categoricalOptionalityObject)
@@ -60,59 +37,69 @@ function step2_getNeededFirstClassObjects(categoricalOptionalityObject) {
     }
     let found = {}
     for (let k in seen) {
-        if (headwaters.includes(k)) {
+        if (everything.hasOwnProperty(k)) {
             found[k] = everything[k]
         }
     }
     return found
-
-
-
 }
 
-function lowerCaseLeadingKey(str) {
-    const components = str.split('.');
-    components[0] = components[0].toLowerCase();
-    return components.join('.');
-  }
 
 function step3_getNonCategoricalObjects(thing, parent, history, loop, result) {
     if (parent.length > 0) {
         history += parent + "."
     }
     if (typeof thing === "object") {
-
         for (let k in thing) {
-
             if (thing[k] !== undefined && k !== "zodValidationFn") {
                 step3_getNonCategoricalObjects(thing[k], k, history, ++loop, result)
             }
         }
     } else {
-        if ( parent !== "zodValidType") {
+        if (parent !== "zodValidType") {
             history = history.slice(0, -1); // Zap the trailing '.'
             history = lowerCaseLeadingKey(history)
-            result[history] = { type: thing, parent, "firstClass":true }
+            result[history] = { type: thing, parent }
         }
     }
 }
 
 
 
-function product_interaction() {
-    const all = everything["categoricalOptionalityObjects"]["product-interaction"]
+
+function examineSomething(eventName) {
+    console.log(eventName)
+    const all = everything["categoricalOptionalityObjects"][eventName]
     let result = {}
-    step1_roll(all, "", "", 0, result)
-    // console.log( result )
+    step1_getCategoricalOptionalityObjects(all, "", "", 0, result)
     const firstClassObjects = step2_getNeededFirstClassObjects(result)
     const seenObjects = {}
     for (let k in firstClassObjects) {
         seenObjects[k] = {}
         step3_getNonCategoricalObjects(firstClassObjects, "", "", 0, seenObjects[k])
     }
-    console.log( seenObjects)
+    // console.log("non-categorical...")
+    // let i = 0 
+    // for ( let k in seenObjects ) { 
+    //     console.log( ++i + "    " + len(seenObjects[k] )) 
+    // }
+
+    // console.log("categorical... ")
+
+    // // i = 0 
+    // // for ( let k in result ) { 
+    // //     console.log( ++i + "    " + len(result[k] )) 
+    // // }
+
+    console.log( result )
+    console.log("LEN=" + len(result))
 }
 
-
-init()
-product_interaction()
+examineSomething("product-interaction")
+examineSomething("purchase")
+examineSomething("page-view")
+examineSomething("page-products-displayed")
+examineSomething("page-products-displayed")
+examineSomething("general-component-event")
+examineSomething("general-component-interaction")
+examineSomething("app-response")
