e92ddcf92b7d411129157a0c7fa8dcbd0ad6a93c
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-04-24 11:57:05 -0700

    working it

--- README.md
+++ README.md
@@ -15,4 +15,7 @@ PASS  4 of 8 flatten_test n=456
 PASS  5 of 8 inflateFlatMap_complex_test  
 PASS  6 of 8 inflateFlatMap_test  
 PASS  7 of 8 getColorizableHOH_test  
-PASS  8 of 8 colorize_test  
+PASS  8 of 8 colorize_test
+
+
+
---
+++ y.js
@@ -0,0 +1,8 @@
+{
+    "Maggy likes to bark.",
+    "Mr. C licks his fur and then coughs", 
+    "Eeboo enjoy fetching balls",
+    "Shabone plays with frisbees",
+    "Frank has short hair and can jump on tables",
+    "Sisko chases mice"
+}
