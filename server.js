2572c893e3c9b3b3e0c8fa356d69972708944845
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-04-24 12:34:03 -0700

    work it

--- server.js
+++ server.js
@@ -3,31 +3,21 @@ const path = require('path');
 const app = express();
 const port = 4040
 
-app.get('/bagOfFunctions.js', function(req, res) {
-  console.log("bagOfFunctions.js")
-  res.sendFile(__dirname + '/bagOfFunctions.js');
-});
 app.get('/index.js', function(req, res) {
   console.log("index.js")
   res.sendFile(path.join(__dirname, '/index.js'));
 });
 
-app.get('/lookatme', function(req, res) {
-  console.log("lookatme")
-  res.sendFile(path.join(__dirname, '/lookatme.html'));
+app.get('/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
-app.get('/hello', function(req, res) {
-  console.log("hello")
-  res.sendFile(path.join(__dirname, '/hello.html'));
+app.get('/index', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
-app.get('/logic.js', function(req, res) {
-  res.sendFile(path.join(__dirname, '/logic.js'));
-});
-
 app.get('/style.css', function(req, res) {
   res.sendFile(path.join(__dirname, '/style.css'));
 });
