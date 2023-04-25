7e0dd5e4398d4686187007ae68ed3e67a42e8b42
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-04-25 08:17:04 -0700

    Cleanup

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("This file is here because, and only because, stackblizt wants it. This is index.js") 
+console.log("%c This file is here because, and only because, stackblizt wants it. This is index.js", "background:tan") 
